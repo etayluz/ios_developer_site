25b31a0449dd68cfd1c5f48500be1dc103afd770
Etay Luz <etayluz@gmail.com>
2014-02-03 17:25:11 -0500

    Add contact emailing

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+
+  $email_to = "etayluz@gmail.com";
+  $email_subject = "New Contact";
+  error_log("here");
+  $name = $_POST['contactName'];
+  $email_from = $_POST['email'];
+  $email_message = "Name: ".$name."\n\n".$_POST['comments'];
+  error_log($email_message);
+  // create email headers
+  $headers = 'From: '.$email_from."\r\n".
+  'Reply-To: '.$email_from."\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+
+  // send email
+  @mail($email_to, $email_subject, $email_message, $headers);
+?>
